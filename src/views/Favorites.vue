346291bcc0216ce5f8e9112e0d0b438a185b5a71
Matrix <jakoo@MatrixdeMacBook-Pro-2.local>
2019-04-24 18:35:59 +0800

    update request

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -1,5 +1,54 @@
 <template>
-  <div class="fav">
-    <h1>This is an Favorite page</h1>
+  <div class="fav" id="fav">
+    <h1>Bitcoin Price Index</h1>
+    <section v-if="errored">
+      <p>We're sorry, we're not able to retrieve this information at the moment, please try back later</p>
+    </section>
+    <section v-else>
+      <div v-if="loading">loading...</div>
+    
+    <div
+      v-for="currency in info" :key="currency"
+      class="currency"
+    >
+      {{ currency.description }}:
+      <span class="lighten">
+        <span v-html="currency.symbol"></span>{{ currency.rate_float | currencydecimal }}
+      </span>
+
+    </div>
+    </section>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+import { error } from 'util';
+export default {
+  data () {
+    return {
+      info: null,
+      loading: true,
+      errored: false
+    }
+  },
+  mounted () {
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => {
+        this.info = response.data.bpi
+      })
+      .catch(error => {
+        console.log(error)
+        this.errored = true
+      })
+      .finally(() => this.loading = false)
+  },
+  filters: {
+    currencydecimal (value) {
+      return value.toFixed(2)
+    }
+  }
+  
+}
+</script>
